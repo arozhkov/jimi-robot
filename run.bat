9931142487b860ae29c5746280083df1f28988a0
arozhkov <alexander.rozhkov@gmail.com>
2012-12-02 19:13:54 +0100

    working run.bat

--- run.bat
+++ run.bat
@@ -1,34 +1,29 @@
 @echo off
 setlocal
 
-rem set JAVA_HOME=c:\java\jdk1.6_35
-rem set JIMI_HOME=c:\jimi
-rem set WLS_LIBS=c:\Oracle\Middleware\wlserver_10.3\server\lib
+set JIMI_HOME=G:\bin\jimi-0.0.1
+set WLS_LIBS=%JIMI_HOME%\lib
 
-set JAVA_OPTS=-Duser.timezone=GMT
-
-if "%JAVA_HOME%"=="" (
-    echo "Undefined JAVA_HOME"
-    exit 1
-)
+set JAVA_OPTS="-Duser.timezone=GMT "
 
 if "%JIMI_HOME%"=="" (
     echo "Undefined JIMI_HOME"
     exit 1
 )
 
-set JIMI_JAR=%JIMI_HOME%\jimi-0.0.1-SNAPSHOT.jar
+set JIMI_JAR=%JIMI_HOME%\jimi.jar
 
-set CLASSPATH=%JIMI_HOME%\
+set CLASSPATH=%JIMI_HOME%\.
 set CLASSPATH=%CLASSPATH%;%JIMI_HOME%\lib\log4j-1.2.16.jar
 set CLASSPATH=%CLASSPATH%;%JIMI_HOME%\lib\slf4j-api-1.7.1.jar
 set CLASSPATH=%CLASSPATH%;%JIMI_HOME%\lib\slf4j-log4j12-1.7.1.jar
 set CLASSPATH=%CLASSPATH%;%JIMI_HOME%\lib\snakeyaml-1.10.jar
 
 if NOT "%WLS_LIBS%"=="" (
-    set CLASSPATH=%CLASSPATH%;%WLS_LIBS%\wlclient.jar
     set CLASSPATH=%CLASSPATH%;%WLS_LIBS%\wljmxclient.jar
 )
+set CLASSPATH=%CLASSPATH%;%JIMI_JAR%
+
 
 if NOT %JAVA_OPTS%=="" (
   set JIMI_CMD=%JAVA_OPTS% -cp %CLASSPATH%;%JIMI_JAR% com.opshack.jimi.Jimi %*
@@ -37,13 +32,11 @@ if NOT %JAVA_OPTS%=="" (
 )
 
 echo.
-echo Start jimi with
-echo JAVA_HOME = %JAVA_HOME%
 echo JIMI_HOME = %JIMI_HOME%
 echo JAVA_OPTS = %JAVA_OPTS%
 echo CLASSPATH = %CLASSPATH%
 echo.
 
-"%JAVA_HOME%\bin\java.exe" %JIMI_CMD% 
+java.exe %JIMI_CMD% 
 
 endlocal
