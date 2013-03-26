4e95b11d2c073d76ca7a7058a34afeb64af06cb5
arozhkov <alexander.rozhkov@gmail.com>
2013-03-26 23:06:59 +0100

    Graphite writer

--- src/main/java/com/opshack/jimi/writers/Graphite.java
+++ src/main/java/com/opshack/jimi/writers/Graphite.java
@@ -1,11 +1,14 @@
 package com.opshack.jimi.writers;
 
+import java.io.StringWriter;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
 import java.net.SocketException;
 import java.net.UnknownHostException;
 
+import org.apache.velocity.VelocityContext;
+import org.apache.velocity.app.VelocityEngine;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -17,9 +20,11 @@ public class Graphite extends Writer {
 	
 	private String host;
 	private int port;
+	private String format;
 	
 	InetAddress address;
 	private DatagramSocket datagramSocket;
+	private VelocityEngine ve = new VelocityEngine();
 	
 	
 	@Override
@@ -39,7 +44,10 @@ public class Graphite extends Writer {
 			return false;
 		}
 		
-		log.info(this.host + ":" + this.port);
+		ve.setProperty(VelocityEngine.RUNTIME_LOG_LOGSYSTEM_CLASS, "org.apache.velocity.runtime.log.NullLogSystem");
+		ve.init();
+		
+		log.info(this.host + ":" + this.port + ", format " + this.format);
 		return true;
 	}
 	
@@ -47,10 +55,22 @@ public class Graphite extends Writer {
 	@Override
 	public void write(Event event) {
 		
-		String stringMessage = event.getSource() + "." + event.getMetric()
-				+ " " + event.getValue() 
-				+ " " + event.getTs()/1000; // divide by 1000 to get seconds
+		//String stringMessage = event.getSource() + "." + event.getMetric()
+		//		+ " " + event.getValue() 
+		//		+ " " + event.getTs()/1000; // divide by 1000 to get seconds
 		
+		VelocityContext velocityContext = new VelocityContext();
+
+		velocityContext.put("source", event.getSource());
+		velocityContext.put("metric", event.getMetric());
+		velocityContext.put("value", event.getValue());
+		velocityContext.put("ts", event.getTs()/1000);
+		
+        StringWriter w = new StringWriter();
+        ve.evaluate(velocityContext, w, "velocity", this.getFormat());
+        
+        String stringMessage =  w.toString();
+        
 		log.debug("Graphite: " + event.getId() + " " + stringMessage + " to " + this.address + ":" + this.port);
 		
 		byte[] byteMessage = stringMessage.getBytes();
@@ -81,4 +101,14 @@ public class Graphite extends Writer {
 	public void setPort(int port) {
 		this.port = port;
 	}
+
+	public String getFormat() {
+		return format;
+	}
+
+	public void setFormat(String format) {
+		this.format = format;
+	}
+	
+	
 }
