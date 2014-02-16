db88e3dc06f78304b9cbff41342497f16b08ee95
arozhkov <alexander.rozhkov@gmail.com>
2014-02-16 21:42:09 +0100

    classpath fix

--- run.sh
+++ run.sh
@@ -6,7 +6,7 @@ JIMI_NAME=${1:-"jimi"}
 JAVA_OPTS="-Djimi.home=${JIMI_HOME} -Djimi.name=${JIMI_NAME} -Duser.timezone=GMT -Djava.security.policy=file:./jimi.policy"
 JIMI_JAR=${JIMI_HOME}/jimi.jar
 
-CLASSPATH=$JIMI_HOME/.:$JIMI_HOME/lib/*.jar
+CLASSPATH=$JIMI_HOME/.:$JIMI_HOME/lib/*
 
 JIMI_CMD="${JAVA_OPTS} -cp ${CLASSPATH}:${JIMI_JAR} com.opshack.jimi.Jimi ${JIMI_HOME}/config/${JIMI_NAME}.yaml"
 
